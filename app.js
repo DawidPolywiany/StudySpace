a0adda3aced1c4008d90bcd5188a39961a367d1c
Dawid <129754596+DawidPolywiany@users.noreply.github.com>
2025-02-02 23:58:46 +0100

    Added App.Js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const _app = express();
+const _port = 3000;
 
+//ejs параметры
+_app.set("view engine", "ejs");
+_app.use(express.static("public"));
+
+//подключаем роуты
+_app.use(require("./routes/main"));
+_app.use(require("./routes/index"));
+
+//запуск сервера
+try {
+    _app.listen(_port, () => {
+        console.log(`сервер запущен на http://localhost:${_port}`);
+    });
+} catch (_err) {
+    console.log('ошибка при запуске сервера:', _err);
+}
